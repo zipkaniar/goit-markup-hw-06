098a2cb940837c6e09b49e4c6500d7455a56ae80
Murat Çolak <mo939614@gmail.com>
2024-06-09 00:56:47 +0300

    java in focus in styles mobil menü in.

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggle() {
+  var modal = document.getElementById("modal-window");
+  modal.classList.toggle("is-open");
+}
+
+function toggleMenu() {
+  var menu = document.getElementById("mobile-hamburg");
+  menu.classList.toggle("is-open");
+}
